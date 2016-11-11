551a399b9942f1da89c7f7619d565af07e101aaf
Zachary Smith <william.smith1@wipro.com>
2016-11-11 16:30:31 -0500

    sudoify the refresh script

--- refresh-style-guide.sh
+++ refresh-style-guide.sh
@@ -1,7 +1,7 @@
 echo "Refreshing style guide"
 
 cd /var/www/style-guide
-git pull
-rm -rf node_modules/
-npm install
-npm run build
+sudo git pull
+sudo rm -rf node_modules/
+sudo npm install
+sudo npm run build
